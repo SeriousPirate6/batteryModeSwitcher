2b02ab5c28c18507fb92e677511123092beecd88
Serious Pirate <whatyoudontexpect3@gmail.com>
2021-10-16 00:08:13 +0200

    Updated and exported documentation

--- batteryAutoSwitcher.bat
+++ batteryAutoSwitcher.bat
@@ -1,20 +1,5 @@
 @echo off
 
-@REM powercfg /L # to see all the power management profiles
-@REM powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
-
-@REM High perf: 9df557d5-8a11-42bf-88e4-c9737e89d41d
-@REM Balanced:  381b4222-f694-41f0-9685-ff5bb260df2e
-@REM WMIC Path Win32_Battery Get BatteryStatus
-	@REM 1 - not in charge
-	@REM 2 - in charge
-@REM WMIC PATH Win32_Battery Get EstimatedChargeRemaining
-
-@REM powercfg -ALIASES # Lists named aliases for available profiles.
-@REM powercfg -S SCHEME_MIN #Activates the (High-Performance) Scheme with the alias SCHEME_MIN
-@REM powercfg -S SCHEME_MAX #Activates the (Max-Energie-Saving) Scheme with the alias SCHEME_MAX
-@REM powercfg -S SCHEME_BALANCED # ... Balanced Energie Scheme
-
 goto start
 
 :repeat
