ac0606c6c8045dd5cfa98c0abf720d1bb5001b9f
Serious Pirate <whatyoudontexpect3@gmail.com>
2021-10-16 01:57:03 +0200

    Bug fix on internal loop and added log function

--- batteryAutoSwitcher.bat
+++ batteryAutoSwitcher.bat
@@ -3,11 +3,18 @@
 goto start
 
 :repeat
+REM TODO check if the variable "interval" isn't null
 timeout /t "%interval%" /nobreak>nul
 
 ENDLOCAL
-set startingSaving=75
+set startingSaving=30
 set interval=5
+set logPath=C:\Users\%USERNAME%\batterySwitcherLogs
+
+for /F "tokens=1" %%i in ('date /t') do set mydate=%%i
+set dd=%mydate:~6,4%-%mydate:~3,2%-%mydate:~0,2%
+
+if not exist "%logPath%" mkdir "%logPath%"
 
 :start
 
@@ -20,27 +27,30 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`WMIC Path Win32_Battery Get BatteryStatus`) 
 
 set /a chargingType="%var2%"
 
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`WMIC Path Win32_Battery Get EstimatedChargeRemaining`) DO (
+SET var!count!=%%F
+SET /a count=!count!+1
+)
+
+set /a batteryRem="%var2%"
+
 if "%chargingType%"=="1" (
-	SET count=1
-	FOR /F "tokens=* USEBACKQ" %%F IN (`WMIC Path Win32_Battery Get EstimatedChargeRemaining`) DO (
-	SET var!count!=%%F
-	SET /a count=!count!+1
-	)
-
-	set /a batteryRem="%var2%"
-
 	if "%batteryRem%" LSS "%startingSaving%" (
-		echo Activating battery saving...
+		echo Activating battery saving... Battery lasting: %batteryRem% Start saving at: %startingSaving%
 		@REM powercfg -S SCHEME_MAX
+		ECHO %date%-%time%: Switch to saving battery profile>>"%logPath%\%dd%.log"
 		goto repeat
 	) else (
 		echo Battery more than %startingSaving%%%.
+		ECHO %date%-%time%: Battery more than %startingSaving%%%>>"%logPath%\%dd%.log"
 		goto repeat
 	)
 )
 
 if "%chargingType%"=="2" (
 	echo Battery on charge.
+	ECHO %date%-%time%: Battery on charge>>"%logPath%\%dd%.log"
 	goto repeat
 )
 
