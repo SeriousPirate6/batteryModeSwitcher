5e0869565ce160703eea7eda72e03a84cab4a042
Serious Pirate <whatyoudontexpect3@gmail.com>
2021-10-17 02:37:36 +0200

    Functionality and updates:
    -added profiles id variables;
    -removed some unuseful controls;
    -added control on which mode is currently active, to avoid profile switching if not required;
    -changed log syntax;
    -updated documentation;

--- batteryAutoSwitcher.bat
+++ batteryAutoSwitcher.bat
@@ -8,12 +8,21 @@ set startingSaving=70
 set interval=5
 set logPath=C:\Users\%USERNAME%\batterySwitcherLogs
 
+set powerSaving=a1841308-3541-4fab-bc81-f71556f20b4a
+set maxPerformance=9df557d5-8a11-42bf-88e4-c9737e89d41d
+
 for /F "tokens=1" %%i in ('date /t') do set mydate=%%i
 set dd=%mydate:~6,4%-%mydate:~3,2%-%mydate:~0,2%
 
 if not exist "%logPath%" mkdir "%logPath%"
 
+FOR /F "tokens=* USEBACKQ" %%F IN (`powercfg /GETACTIVESCHEME`) DO (
+SET var=%%F
+)
+set currentBatteryScheme=%var%
+
 SETLOCAL ENABLEDELAYEDEXPANSION
+
 SET count=1
 FOR /F "tokens=* USEBACKQ" %%F IN (`WMIC Path Win32_Battery Get BatteryStatus`) DO (
   SET var!count!=%%F
@@ -29,7 +38,7 @@ set /a chargingType="%var2%"
 if "%chargingType%"=="1" (
 	goto proceed
 ) else (
-	goto end
+	goto maxperf
 )
 
 :proceed
@@ -46,25 +55,39 @@ set /a batteryRem="%var2%"
 @REM set /a batteryRem=%startingSaving%
 
 if %batteryRem% LSS %startingSaving% (
-	echo Activating battery saving... Battery lasting: %batteryRem% Start saving at: %startingSaving%
-	REM powercfg -S SCHEME_MAX
-	ECHO %date%-%time%: Switch to saving battery profile>>"%logPath%\%dd%.log"
-	goto repeat
+	goto saving
 ) else (
-	if not [%startingSaving%] == [] (
-		echo Battery more than %startingSaving%%%.
-		ECHO %date%-%time%: Battery more than %startingSaving%%%>>"%logPath%\%dd%.log"
-	)
+	echo Battery more than %startingSaving%%%.
+	ECHO %date%-%time%: Battery more than %startingSaving%%%>>"%logPath%\%dd%.log"
 	goto repeat
 )
 
-:end
+:saving
 
-if "%chargingType%"=="2" (
+echo %currentBatteryScheme%|find "%powerSaving%" >nul
+if errorlevel 1 (
+	echo Activating battery saving... Battery lasting: %batteryRem% Start saving at: %startingSaving%
+	powercfg -S %powerSaving%
+	ECHO %date%-%time%: Switch to saving battery profile>>"%logPath%\%dd%.log"
+) else (
+	echo Battery saving already activated... Battery lasting: %batteryRem%
+	ECHO %date%-%time%: MODE: battery saving. Battery lasting: %batteryRem% >>"%logPath%\%dd%.log"
+)
+goto repeat
+
+:maxperf
+
+echo %currentBatteryScheme%|find "%maxPerformance%" >nul
+if errorlevel 1 (
+	echo Activating max performance...
+	powercfg -S %maxPerformance%
+	ECHO %date%-%time%: Switch to max performance profile>>"%logPath%\%dd%.log"
+) else (
 	echo Battery on charge.
-	if not [%startingSaving%] == [] ECHO %date%-%time%: Battery on charge>>"%logPath%\%dd%.log"
-	goto repeat
+	ECHO %date%-%time%: MODE: max performace. Battery on charge.>>"%logPath%\%dd%.log"
 )
+goto repeat
+
 
 timeout /t 2 /nobreak>nul
 
